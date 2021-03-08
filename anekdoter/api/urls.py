328efa7fac294b43aa822920688bb30f0ee458db
znifer <trueznifer@gmail.com>
2021-03-08 18:15:54 +0300

    swagger fixes

--- anekdoter/api/urls.py
+++ anekdoter/api/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView)
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
