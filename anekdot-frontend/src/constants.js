c91457124e4e96667f411307f670eda18a51fffd
spAm25 <spAm250799@gmail.com>
2021-03-13 13:35:09 +0300

    Я БУКВУ S ЗАБЫЛ

--- anekdot-frontend/src/constants.js
+++ anekdot-frontend/src/constants.js
@@ -1,6 +1,6 @@
 const constants = {
-  apiBaseURL: 'https://anekdot.neural.ro/api',
-  ttsBaseURL: 'https://anekdot.neural.ro'
+  apiBaseURL: 'https://anekdot.neurals.ro/api',
+  ttsBaseURL: 'https://anekdot.neurals.ro'
 }
 
 export default constants
